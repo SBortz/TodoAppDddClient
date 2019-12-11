da4e3b2b64d50eacb77c4fb75a1ae2816ec3b14a
Sebastian Bortz <sbortzneusta@gmail.com>
2019-12-11 15:50:42 +0100

    Added initial flutter project

---
+++ Source/todo_app_ddd_client/android/app/src/main/kotlin/todoappddd/de/todo_app_ddd_client/MainActivity.kt
@@ -0,0 +1,13 @@
+package todoappddd.de.todo_app_ddd_client
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
